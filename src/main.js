77d98aabc2fcc9637386ed6823c52920d42e30b2
pospi <pospi@spadgos.com>
2016-06-29 14:37:01 +1000

    add solidity syntax highlighting and set as default

--- src/main.js
+++ src/main.js
@@ -8,18 +8,23 @@
 
 import polyfills from 'remark/src/polyfills';
 import Remark from 'remark/src/remark/api';
+import RemarkHighlighter from 'remark/src/remark/highlighter';
 import styler from 'remark/src/remark/components/styler/styler';
 
+import extendHighlighter from './highlighter-extensions';
 import pageContent from '../solidity-notes.md';
 
+
 polyfills.apply();
+extendHighlighter(RemarkHighlighter);
 styler.styleDocument();
+
 const remark = new Remark();
 
 const slideshow = remark.create({
     source: pageContent,
     ratio: '16:9',
-    highlightLanguage: 'javascript',
+    highlightLanguage: 'solidity',
     highlightStyle: 'solarized-light',
 });
 
